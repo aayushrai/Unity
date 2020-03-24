e673cd37acbca03800d10c05d6ef429c49483d07
aayushrai <www.aayushrai@gmail.com>
2020-03-25 02:49:37 +0530

    prototype

---
+++ Prototype 2/Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float HorizontalInput;
+    private float xRange=20;
+    public float PlayerSpeed = 20.0f;
+    public GameObject projectilePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, -transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * HorizontalInput * PlayerSpeed);
+
+        
+    }
+}
