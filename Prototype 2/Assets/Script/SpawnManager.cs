8afa046175356aa629edca10da74f79ad1b23385
aayushrai <www.aayushrai@gmail.com>
2020-04-04 01:58:58 +0530

    generate random animals

---
+++ Prototype 2/Assets/Script/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] animalPrefabs;
+    private float spawnXRange = 20f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 SpawnPos = new Vector3(Random.Range(-spawnXRange, spawnXRange), 0, 20);
+            Instantiate(animalPrefabs[animalIndex], SpawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
